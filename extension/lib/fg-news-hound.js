8fd7754b3fc91a950bccec09aadbb97f3be4d9ed
bcoe <=>
2012-05-02 19:57:43 -0700

    Hooked in highlighting.

--- extension/lib/fg-news-hound.js
+++ extension/lib/fg-news-hound.js
@@ -1,18 +1,45 @@
 function FGNewsHound() {
-	console.log('hello world');
 	var client = new Queuebert.Client({
 		identifier: 'client',
 		delegate: this
 	});
+
+	this.attachEvents();
 }
 
+FGNewsHound.prototype.attachEvents = function() {
+	var _this = this;
+	
+	$('.nh-highlighted').live('click', function() {
+		_this.displayComment( $(this) );
+	});
+};
+
 FGNewsHound.prototype.addNewComment = function(clientId, clientTabId, body) {
-	//var element = $('*:contains(' + body.selectionText + ')').last().
-	//	html = 
+	var smallestElement = null,
+		smallestCount = 99999999;
+		
+	$('*').each(function() {
+		if ($(this).text().indexOf(body.selectionText) > -1) {
+			var html = $(this).html();
+			if (html.length < smallestCount) {
+				smallestCount = html.length;
+				smallestElement = $(this);
+			}
+		}
+	});
+	
+	if (smallestElement) {
+		smallestElement.addClass('nh-highlighted');
+	}
+};
+
+FGNewsHound.prototype.displayComment = function(element) {
+	console.log('should display comment.')
 };
 
 $(document).ready(function() {
 	new FGNewsHound();
+	
 	$('body').append( $('<div id="news-hound-thread" />') );
-	console.log($('#news-hound-thread'));
 });
